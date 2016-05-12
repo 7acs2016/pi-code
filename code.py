222b509a3c907ab6016435f54ba8deaf58cd4902
7acs2016 <09_1608@apps.acs.edu.lb>
2016-05-12 13:10:27 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,11 +10,10 @@ GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(4, GPIO.IN)
-#check if switch is pressed i.e if a plug is inserted in the socket wall
 
-while True:
-    if (GPIO.input(4)==False):
-        GPIO.output(18, GPIO.HIGH)# this will turn ON the red LED       
+while True
+    if (GPIO.input(4)==False): #check if switch is pressed i.e if a plug is inserted in the socket wall
+        GPIO.output(18, GPIO.HIGH)# this will turn ON the Green LED       
                                   # this also means wall socket is LIVE!!
         GPIO.output(17, GPIO.LOW)
         os.system('clear')
